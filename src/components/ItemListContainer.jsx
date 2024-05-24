087109672d4ee8dd16f9436a501045813d098f6f
Daniel Giussani <dngiussani28@gmail.com>
2024-05-24 09:52:22 +0200

    proyectoFinal

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,13 +1,17 @@
 import { useEffect, useState } from "react"; 
-import arrayProductos from "./json/productos.json";
+//import arrayProductos from "./json/productos.json";
 import ItemList from "./ItemList";
 import { useParams } from "react-router-dom";
+import { collection, doc, getDoc, getDocs, getFirestore, query, where } from "firebase/firestore";
 
 const ItemListContainer = () => {
     const [items, setItems] = useState([]);
     const {id} = useParams();
 
-    useEffect(() => {
+
+//Acceso a productos via JSON
+
+    /* useEffect(() => {
         const promesa = new Promise((resolve) => {
             setTimeout(() => {
                 resolve(id ? arrayProductos.filter(item => item.categoria == id) : arrayProductos);
@@ -17,12 +21,57 @@ const ItemListContainer = () => {
         promesa.then(respuesta => {
             setItems(respuesta);
         })
-    }, [id])
+    }, [id]) */
+
+    //Acceso a un producto por ID en Firestore
+    // .id  => accedo al id del documento
+    // .data() => accedo a todos los campos del cocumento
+    /* useEffect (() => {
+        const db = getFirestore();
+        const docRef =  doc(db, "productos", "DQVFdVrbI1tcTUjZ6zQn");
+        getDoc(docRef).then(snapShot => {
+            if (snapShot.exists()) {
+                setItems();
+            }
+        });
+    }, []); */
+
+    //Acceder a una Collection en Firestore
+
+    /* useEffect (() => {
+        const db = getFirestore();
+        const itemsCollection = collection(db, "productos");
+        getDocs(itemsCollection).then(snapShot => {
+            if (snapShot.size > 0){
+                setItems(snapShot.docs.map(item => ({id:item.id, ...item.data()})));
+            }
+        });
+    }, []); */
+
+    // Acceder a una Collection en Firestore mediante una Query
+
+    useEffect (() => {
+        const db = getFirestore();
+        const itemsCollection = collection(db, "productos");
+        //const queryCollection = query(itemsCollection, where("precio", ">" , 25 ));
+        //const queryCollection = query(itemsCollection, where("precio", "<" , 25 ));
+        //const queryCollection = query(itemsCollection, where("nombre", "==" , "Vestido de popelín estampado de niña"));
+        //const queryCollection = query(itemsCollection, where("nombre", "!=" , "Vestido de popelín estampado de niña"));
+        const queryCollection = query(itemsCollection, (where("precio", ">" , 30 ), where("categoria", "==" , "niña")));
+        getDocs(queryCollection).then(snapShot => { 
+            if (snapShot.size > 0){
+                setItems(snapShot.docs.map(item => ({id:item.id, ...item.data()})));
+            }
+        });
+    }, []);
 
     return (
         <div className="container">
             <div className="row my-5">
                 <ItemList items={items} />
+                {/* <p>{items.nombre}</p>
+                <p><img src={items.imagen} alt={items.imagen} /></p>
+                <p>€{items.precio}</p> */}
             </div>
         </div>
     )
--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAxU8TxEjgU54Fkg_Yb3d-X38BRzk8uZAc",
+    authDomain: "pikabu-moda-infantil.firebaseapp.com",
+    projectId: "pikabu-moda-infantil",
+    storageBucket: "pikabu-moda-infantil.appspot.com",
+    messagingSenderId: "265559307464",
+    appId: "1:265559307464:web:461fc7d36887f7ee8921fd"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 ReactDOM.createRoot(document.getElementById('root')).render(<App />)
