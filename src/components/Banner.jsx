1d450509c95890f4cdec90a33d2a994f2318026f
Daniel Giussani <dngiussani28@gmail.com>
2024-05-30 01:58:41 +0200

    Se hacen modificaciones para que sea responsive

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,27 +6,27 @@ import bannerAccesorios from "../assets/images/bannerAccesorios.jpg";
 
 const Banner = () => {
     return (
-        <div className="container">
+        <div className="container-fluid">
             <div className="row">
-                <div className="col d-flex align-items-center justify-content-center" style={{ backgroundImage: `url(${bannerNewBorn})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", opacity: 0.9, height: 320 }}>
+                <div className="col-12 col-md-3 d-flex align-items-center justify-content-center" style={{ backgroundImage: `url(${bannerNewBorn})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", opacity: 0.9, height: "20rem" }}>
                     <Link to={"/category/newborn"} className="text-decoration-none">
                         <span className="bg-white text-uppercase px-3 py-2" style={{ color: "#D96704" }}>Recién Nacido</span>
                     </Link>
                 </div>
 
-                <div className="col d-flex align-items-center justify-content-center" style={{ backgroundImage: `url(${bannerNinia})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", opacity: 0.9, height: 320 }}>
+                <div className="col-12 col-md-3 d-flex align-items-center justify-content-center" style={{ backgroundImage: `url(${bannerNinia})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", opacity: 0.9, height: "20rem" }}>
                     <Link to={"/category/niña"} className="text-decoration-none">
                         <span className="bg-white text-uppercase px-3 py-2" style={{ color: "#D96704" }}>Niña</span>
                     </Link>
                 </div>
 
-                <div className="col d-flex align-items-center justify-content-center" style={{ backgroundImage: `url(${bannerNinio})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", opacity: 0.9, height: 320 }}>
+                <div className="col-12 col-md-3 d-flex align-items-center justify-content-center" style={{ backgroundImage: `url(${bannerNinio})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", opacity: 0.9, height: "20rem" }}>
                     <Link to={"/category/niño"} className="text-decoration-none">
                         <span className="bg-white text-uppercase px-3 py-2" style={{ color: "#D96704" }}>Niño</span>
                     </Link>
                 </div>
 
-                <div className="col d-flex align-items-center justify-content-center" style={{ backgroundImage: `url(${bannerAccesorios})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", opacity: 0.9, height: 320 }}>
+                <div className="col-12 col-md-3 d-flex align-items-center justify-content-center" style={{ backgroundImage: `url(${bannerAccesorios})`, backgroundRepeat: "no-repeat", backgroundSize: "cover", opacity: 0.9, height: "20rem" }}>
                     <Link to={"/category/accesorios"} className="text-decoration-none">
                         <span className="bg-white text-uppercase px-3 py-2" style={{ color: "#D96704" }}>Accesorios</span>
                     </Link>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,16 +2,16 @@ import logoFooter from "../assets/images/logoFooter.png";
 
 const Footer = () => {
     return (
-        <div className="container-fluid"style={{ backgroundColor: "#D96704" }}>
-            <div className="row mt-5 p-3" >
-                <div className="col textoFooter text-white text-center">
-                    <img src={logoFooter} alt="logo pikabu, un zorrito sonriendo" style={{ width: 100, height: 100 }} className="mb-2" />
+        <div className="container-fluid text-white mt-5 p-3" style={{ backgroundColor: "#D96704"}}>
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-6 text-center">
+                    <img src={logoFooter} alt="logo pikabu, un zorrito sonriendo" style={{ width: "100px", height: "100px" }} className="mb-2" />
                     <h5><b>«He aquí mi secreto, que no puede ser más simple: solo con el corazón se puede ver bien; lo esencial es invisible a los ojos»</b></h5>
                 </div>
             </div>
-            <div className="row">
-                <div className="col textoFooter text-white text-center">
-                    <p>Copyright © Pikabú Ropa Infantil - 2024. Todos los derechos reservados.</p>
+            <div className="row justify-content-center">
+                <div className="col-12 text-center">
+                    <p>Derechos de autor © Pikabú Ropa Infantil - 2024. Todos los derechos reservados.</p>
                 </div>
             </div>
         </div>
